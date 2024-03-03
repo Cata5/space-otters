9315547e940a835cbb38178cfabeb1144b714139
Cata5 <gogu9194@gmail.com>
2024-03-03 13:29:32 +0200

    Merge branch 'master' of https://github.com/Cata5/space-otters

--- components/robot.tsx
+++ components/robot.tsx
@@ -6,7 +6,7 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 
 
 const Model = () => {
-  const gltf = useLoader(GLTFLoader, "./robot2.gltf");
+  const gltf = useLoader(GLTFLoader, "./robot.gltf");
 
   return (
     <>

6ff175e073f387ac307e48f9638d1ec50832a1b4
Cata5 <gogu9194@gmail.com>
2024-03-09 14:46:42 +0200

    Update

--- app/evenimente/scoli/page.tsx
+++ app/evenimente/scoli/page.tsx
@@ -36,26 +36,51 @@ export default function Scoli() {
   }, []);
 
   return (
-    <div>
-      <div className="flex items-center justify-center mx-[1.75rem] xl:m-auto flex-col max-w-7xl">
-        <h1 className="text-3xl sm:text-5xl py-4">Vizite Scoli Pitesti</h1>
-        <div className="py-16 flex flex-col gap-8">
-          <h1 className="text-center sm:text-left text-3xl sm:text-5xl">Introducere</h1>
-          <div
-            className="text-md text-center sm:text-xl sm:text-justify whitespace-pre-wrap"
-            dangerouslySetInnerHTML={{ __html: introContent || "" }}
-          />
-          <h1 className="text-center sm:text-left text-3xl sm:text-5xl">Scop</h1>
-          <div
-            className="text-md text-center sm:text-xl sm:text-justify whitespace-pre-wrap"
-            dangerouslySetInnerHTML={{ __html: scopContent || "" }}
-          />
-          <h1 className="text-center sm:text-left text-3xl sm:text-5xl">Desfasurare</h1>
-          <div
-            className="text-md text-center sm:text-xl sm:text-justify whitespace-pre-wrap"
-            dangerouslySetInnerHTML={{ __html: desfContent || "" }}
-          />
+    // <div>
+    //   <div className="flex items-center justify-center mx-[1.75rem] xl:m-auto flex-row max-w-7xl">
+    //     <h1 className="text-3xl sm:text-5xl py-4">Vizite Scoli Pitesti</h1>
+    //     <div className="py-16 flex flex-col  gap-8">
+    //       <h1 className="text-center sm:text-left text-3xl sm:text-5xl">Introducere</h1>
+    //       <div
+    //         className="text-md text-center max-w-3xl sm:text-xl sm:text-justify whitespace-pre-wrap"
+    //         dangerouslySetInnerHTML={{ __html: introContent || "" }}
+    //       />
+    //       <h1 className="text-center sm:text-left text-3xl sm:text-5xl">Scop</h1>
+    //       <div
+    //         className="text-md text-center max-w-3xl sm:text-xl sm:text-justify whitespace-pre-wrap"
+    //         dangerouslySetInnerHTML={{ __html: scopContent || "" }}
+    //       />
+    //       <h1 className="text-center sm:text-left text-3xl sm:text-5xl">Desfasurare</h1>
+    //       <div
+    //         className="text-md text-center max-w-3xl sm:text-xl sm:text-justify whitespace-pre-wrap"
+    //         dangerouslySetInnerHTML={{ __html: desfContent || "" }}
+    //       />
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="flex items-center flex-col gap-[3rem] py-[1rem] mx-[2rem] md:m-auto min-h-screen">
+      <h1 className="text-3xl sm:text-5xl py-4">Vizite Scoli Pitesti</h1>
+      <div className="flex xl:flex-row flex-col max-w-7xl gap-[1rem]">
+        <div className="flex items-center flex-col max-w-3xl">
+          <h1 className="text-center sm:text-left text-3xl sm:text-5xl my-[3rem]">Introducere</h1>
+          <p className="text-center whitespace-pre-wrap"> {introContent}</p>
         </div>
+        <div className="flex items-center flex-col max-w-3xl">
+          <h1 className="text-center sm:text-left text-3xl sm:text-5xl my-[3rem]">Scop</h1>
+          <p className="text-center whitespace-pre-wrap">{scopContent}</p>
+        </div>
+      </div>
+      <div className="flex flex-col items-center  justify-center">
+        <h1 className="text-center text-3xl sm:text-5xl my-3">Desfasurare</h1>
+        {desfContent !== null && (
+          <div className="flex flex-wrap justify-center gap-[2rem] m-auto">
+            {desfContent.split('\n').map((paragraph, index) => (
+              <p key={index} className=" text-center max-w-md whitespace-pre-wrap my-2">
+                {paragraph}
+              </p>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );

19d180eac10af6232149bf5d6b6b4b2013527121
Edy4ever6 <edy4ever6@gmail.com>
2024-03-19 13:02:01 +0200

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
-"use client";
+'use client'
+import { useEffect } from 'react';
+import { useState } from 'react';
 import "@/styles/globals.css";
 import Sponsors from "@/components/sponsors";
 import Members from "@/components/members";
@@ -8,6 +10,7 @@ import { siteConfig } from "@/config/site";
 import localFont from "@next/font/local";
 import Navbar from "@/components/navbar";
 import Image from "next/image";
+
 const roca = localFont({
   src: [
     {
@@ -19,10 +22,14 @@ const roca = localFont({
 });
 
 export default function Home() {
+
   return (
-    <main>
+    <main className='min-h-screen'>
       <Navbar />
-      <Image className='!border-0 py-[10rem]' src='./logo/robot.png' width={800} height={600} alt=""/>
+      <div className="flex max-w-screen gap-[-2rem] items-center py-[10rem]">
+        <Image className='!border-0' src='./logo/robot.png' width={800} height={600} alt=""/>
+        <Image className='!border-0 mr-[1rem] max-w-2xl' id="animatedImage" src='./logo/Logo_Space_light.png' width={900} height={400} alt=""/>
+      </div>
       {siteConfig.navItems.map((item) => (
         <div key={item.href} id={item.href} className="flex items-center justify-center py-[5rem] ">
           {item.href === "obiective" && <Obiective />}
